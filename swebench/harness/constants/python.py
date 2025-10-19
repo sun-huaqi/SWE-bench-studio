5cc93c262a82ce35c2188cc6887e28cbb0cdf631
sun00max <sun00max@gmail.com>
2025-10-20 00:04:05 +0200

    evaluate browser-use__browser-use-2512

--- swebench/harness/constants/python.py
+++ swebench/harness/constants/python.py
@@ -909,7 +909,7 @@ SPECS_BROWSER_USE = {
         "pip_packages": ["uv"],
         "test_cmd": "uv run pytest -rA",
     }
-    for k in ["0.5.9"]
+    for k in ["0.5.9", "0.5.5"]
 }
 
 # Constants - Task Instance Instllation Environment
